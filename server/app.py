f039e257402600e38d17011e1568d4fa2142614f
Nelson Mbau <mbau.ndungu@gmail.com>
2024-10-07 09:42:08 +0300

    feat: add model, migrate database, add routes

--- server/app.py
+++ server/app.py
@@ -1,13 +1,15 @@
 # server/app.py
 #!/usr/bin/env python3
-
+import os
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
 from models import db, Earthquake
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db_path = os.path.join(os.path.dirname(__file__), 'instance', 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -21,7 +23,17 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake(id):
+    data = Earthquake.query.filter(Earthquake.id == id).first()
+    if data:
+        response = data.to_dict()
+        status = 200
+    else:
+        response = {'message': f'Earthquake {id} not found.'}
+        status = 404
 
+    return make_response(response, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
